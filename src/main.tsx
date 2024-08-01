625623364d1af32877e0746cd6110ed4b34bd48c
Jakub <kubalew.pl@gmail.com>
2024-08-01 11:01:02 +0200

    feat: create http service

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,13 @@ import "./index.css";
 import Router from "./router";
 import { theme } from "./theme";
 import { ThemeProvider } from "@mui/material";
+import { HttpService } from "./query-client-provider";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Router />
+      <HttpService>
+        <Router />
+      </HttpService>
     </ThemeProvider>
   </React.StrictMode>
 );
